691eea4d4b31aa269420a9b6ba192e8416128a21
Pawan Choubey <pawanchoubey408@gmail.com>
2025-12-28 10:14:44 +0530

    User Routes and Controllers Added

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -7,14 +7,11 @@ export const protectRoute=async(req,res,next)=>{
         if(!token){
             return res.status(401).json({error:"Unauthorised: No Token Provided"});
         }
-        console.log("token is :",token);
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
         if(!decoded){
             return res.status(401).json({error:"Invalid Token"});
         }
-        console.log("decoded is : ",decoded);
         const user=await User.findById(decoded.id).select("-password");
-        console.log("user is : ",user);
         if(!user){
             return res.status(404).json({error:"User not found"});
         }
@@ -23,7 +20,6 @@ export const protectRoute=async(req,res,next)=>{
     }
     catch(error){
         console.log("Error in protectRoute middleware",error.message);
-        console.log("jwt is : ".jwt);
         return res.status(500).json({error:"Internal Server Error"});
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import authRoutes from './routes/auth.routes.js';
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/user.routes.js'
+import {v2 as cloudinary} from "cloudinary"
 
 const app=express();
 app.use(express.json());
@@ -10,8 +12,15 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true})); // to parse URL-encoded data
 const PORT=process.env.PORT || 3000;
 dotenv.config();
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+}
+);
 
 app.use('/api/auth',authRoutes);
+app.use('/api/users',userRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
